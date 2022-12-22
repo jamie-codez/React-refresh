5ace052e3d97270dde833147928e7fe46b6c99b8
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-22 11:56:04 +0300

    Prop types for properties

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
+import PropTypes from 'prop-types'
+
 const Header = ({ title }) => {
   return (
     <header>{title}</header>
   )
 }
+
 Header.defaultProps = {
     title: 'Task tracker'
+}
+
+Header.propTypes={
+    title:PropTypes.string
 }
 
 export default Header
