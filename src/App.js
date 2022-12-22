9c0fad6a6a5edd1a4521f5c9bbeb3e39d3474f8d
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-22 11:44:07 +0300

    Turnary operations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
- 
 function App() {
-  const name = 'James'
+  const name = "James";
+  const x = true;
   return (
     <div className="container">
       <h1>Hello from React</h1>
       <h2>Hello {name}</h2>
+      <h2>{x ? "Yes" : "No"}</h2>
     </div>
   );
 }
