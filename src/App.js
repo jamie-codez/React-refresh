f4932399522a365e6653a37fbc428522d7b91e75
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-22 15:19:30 +0300

    Done with UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 function App() {
+  const [showAddTask,setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -47,8 +48,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd={addTask}/>
+      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask &&<AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
