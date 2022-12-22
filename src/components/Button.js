78283dd72cc5ac734a54b4591a3bbf84030fae5a
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-22 13:10:58 +0300

    Added events as props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,26 +1,25 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const Button = ({ color, text }) => {
-    const onClick= (e)=>{
-        console.log(e)
-    }
-
-
+const Button = ({ color, text, onClick }) => {
   return (
-    <button onClick={onClick} style={{ backgroundColor: color }} className="btn">
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
       {text}
     </button>
   );
 };
 
 Button.defaultProps = {
-    color:'steelblue'
-}
+  color: "steelblue",
+};
 
 Button.propTypes = {
-    text:PropTypes.string,
-    color:PropTypes.string
-}
-
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick:PropTypes.func 
+};
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,25 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
   return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add'/>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-    title: 'Task tracker'
-}
+  title: "Task tracker",
+};
 
-Header.propTypes={
-    title:PropTypes.string.isRequired,
-}
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
-// CSS in JS
-// const headerStyle = {
-//     color:'red',
-//     backgroundColor:'black'
-// }
 
-export default Header
+export default Header;
