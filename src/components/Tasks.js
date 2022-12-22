0c68842c1d38f082a7477a48bd8dab1d61ac872a
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-22 16:04:17 +0300

    Added restful call to code i.e POST,GET,PUT and DELETE to app we will update the whole object later

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,14 @@
 import Task from "./Task";
-const Tasks = ({tasks,onDelete,onToggle}) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        /> // If you get key error here use index eg tasks.map(task,index) ....<Task key={index} .....
       ))}
     </>
   );
